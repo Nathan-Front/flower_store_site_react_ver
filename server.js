0d12ac5e8b3f1270cfb7f7b6bc7fbf6d63072102
Nathan-Front <jonathantindungan3@gmail.com>
2026-08-19 10:06:43 +0900

    finding cors problem for COD

--- server.js
+++ server.js
@@ -46,7 +46,10 @@ app.use(
     credentials: true,
   }),
 );
-
+app.use((req, res, next) => {
+  console.log("🔥 REQUEST:", req.method, req.url);
+  next();
+});
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "..")));
 
